309abcd7ea01272f75826daa3278fa888519c0c7
J. Alberto <45895462+JAlbertoGonzalez@users.noreply.github.com>
2021-04-20 08:46:02 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,138 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { Button, Col, Container, Form, FormGroup, Row } from 'react-bootstrap';
+import { Slider, Table, TableCell, TableRow } from '@material-ui/core';
+import hdkey from 'hdkey'
+import crypto from 'crypto'
+import _ from 'lodash'
+import { DataGrid, GridColDef, GridRowData } from '@material-ui/data-grid';
+import { getNodeId, getQueueOffset } from './utils';
+
 
 function App() {
+
+  const [privateKey, setPrivateKey] = useState<string>(hdkey.fromMasterSeed(Buffer.from('1')).privateExtendedKey);
+  const [totalRenters, setTotalRenters] = useState(10);
+  const [overlap, setOverlap] = useState(1);
+
+  const dataColumns: GridColDef[] = [
+    { field: 'id', headerName: 'ID', width: 70 },
+    { field: 'nodeId', headerName: 'Renter ID', width: 350 },
+    {
+      headerName: 'Rabbit Range',
+      flex: 1,
+      field: 'rabbit',
+      renderCell: (x) => {
+        return <Slider
+          min={0}
+          max={256}
+          marks
+          valueLabelDisplay="auto"
+          aria-labelledby="range-slider"
+          value={[x.row.exchangeByteStart, x.row.exchangeByteEnd]}
+          step={1} />
+      }
+    }
+  ]
+
+  const data: GridRowData[] = _.times(totalRenters, (n) => {
+    const renterPk = hdkey.fromExtendedKey(privateKey).derive('m/' + (n + 1))
+    const nodeId = getNodeId(renterPk);
+    const offset = getQueueOffset(totalRenters, overlap);
+    const exchangeByteStart = Buffer.from(nodeId, 'hex')[0] - offset;
+    const exchangeByteEnd = exchangeByteStart + (offset * 2);
+
+    const start = Buffer.from([exchangeByteStart]).toString('hex')
+    const end = Buffer.from([exchangeByteEnd]).toString('hex')
+
+    return {
+      id: n + 1,
+      nodeId: getNodeId(renterPk),
+      offset: offset,
+      exchangeByteStart: start,
+      exchangeByteEnd: end
+    }
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container fluid>
+      <Form>
+        <FormGroup>
+          <Form.Label>Complex Private Key</Form.Label>
+          <Form.Control value={privateKey} onChange={(e) => {
+            setPrivateKey(e.target.value)
+          }}>
+
+          </Form.Control>
+          <Button onClick={() => {
+            setPrivateKey(hdkey.fromMasterSeed(crypto.randomBytes(16)).privateExtendedKey);
+          }}>Generate a new key</Button>
+        </FormGroup>
+        <Row>
+          <Col>
+
+            <FormGroup>
+              <Form.Label>Total Renters</Form.Label>
+              <Form.Control value={totalRenters} type="number" onChange={(e) => {
+                setTotalRenters(parseInt(e.target.value, 10))
+              }}>
+
+              </Form.Control>
+            </FormGroup>
+          </Col>
+
+          <Col>
+            <FormGroup>
+              <Form.Label>Overlap</Form.Label>
+              <Form.Control value={overlap} type="number" onChange={(e) => {
+                setOverlap(parseInt(e.target.value, 10))
+              }}>
+
+              </Form.Control>
+            </FormGroup>
+          </Col>
+        </Row>
+
+
+        {/*
+        <div style={{ height: 700, width: '100%' }}>
+          <DataGrid
+            rowsPerPageOptions={[256, 512, 1024]}
+            disableSelectionOnClick
+            rows={data} columns={dataColumns}
+            rowHeight={100}
+            pageSize={5} checkboxSelection />
+        </div>
+*/}
+
+        <Table>
+
+          {data.map((row) => {
+            return <TableRow>
+
+              <TableCell width={10}>{row.id}</TableCell>
+              <TableCell width={10}>{row.nodeId}</TableCell>
+              <TableCell width={10}>{row.exchangeByteStart}</TableCell>
+              <TableCell width={10}>{row.exchangeByteEnd}</TableCell>
+              <TableCell>
+                <Slider
+                  marks
+                  min={0}
+                  max={256}
+                  valueLabelDisplay="auto"
+                  aria-labelledby="range-slider"
+                  value={[parseInt(Buffer.from(row.exchangeByteStart).toString(), 16), parseInt(Buffer.from(row.exchangeByteEnd).toString(), 16)]}
+                  step={1} />
+              </TableCell>
+
+            </TableRow>
+
+
+          })}
+        </Table>
+
+      </Form>
+    </Container>
   );
 }
 
---
+++ src/utils.ts
@@ -0,0 +1,33 @@
+import HDNode from 'hdkey';
+import crypto from 'crypto';
+import assert from 'assert';
+
+export function getNodeId(key: HDNode) {
+  const sha256 = crypto.createHash('sha256').update(key.publicExtendedKey).digest();
+  return crypto.createHash('ripemd160').update(sha256).digest('hex').toString();
+}
+
+const _getQueueSpan = function (t: number, y: number, p: number) {
+  assert(Number.isSafeInteger(t) && t > 0, 't is expected to be integer greater than zero');
+  assert(Number.isSafeInteger(y) && y > 0, 'y is expected to be integer greater than zero');
+  assert(Number.isFinite(p) && p > 0 && p < 1, 'p is expected to be number > 0 and < 1');
+
+  var x = (Math.pow((p - 1) / -1, 1 / y) - 1) * -t;
+  return x;
+};
+
+export function getQueueOffset(totalRenters: number, overlap: number) {
+  if (!Number.isFinite(overlap)) {
+    overlap = 1;
+  }
+
+  var span = _getQueueSpan(
+    256, // total number of queues
+    totalRenters, // total number of renters in pool
+    0.999 // percentage
+  );
+
+  return Math.ceil(span * overlap / 2);
+};
+
+
